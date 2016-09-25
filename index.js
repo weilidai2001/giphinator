9264f02c357ff90b906e750dcd42e54e2855bb01
Weili Dai <Weili.Dai@sky.uk>
2016-09-25 14:19:53 +0100

    Functioning without caching

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict'
 
 const express = require('express'),
-    bodyParser = require('body-parser'),
     giphinateHandler = require('./lib/handlers/giphinate'),
     app = express()
 
 app
-.use(bodyParser)
 .get('/:queryText', giphinateHandler)
 
 .listen(process.env.PORT, function () {
