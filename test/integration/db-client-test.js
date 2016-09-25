b87769797f0d842a1582b049ab92302185b42312
Weili Dai <Weili.Dai@sky.uk>
2016-09-25 22:40:33 +0100

    Reformatted according to .jscsrc and .jshintrc

--- test/integration/db-client-test.js
+++ test/integration/db-client-test.js
@@ -1,28 +1,28 @@
-'use strict';
-const should = require("chai").should();
-const Q = require('q');
+'use strict'
+const should = require('chai').should()
+const Q = require('q')
 
-const DBClient = require('../../lib/external-clients/db-client');
+const DBClient = require('../../lib/external-clients/db-client')
 
 const schemas = require('../../lib/schemas')({
   DATABASE_URL: process.env.DATABASE_URL
-});
+})
 
-const dBClient = new DBClient(schemas.Giphys);
+const dBClient = new DBClient(schemas.Giphys)
 
-describe("db-client", function () {
-  it("should retrieve a gif if url already exists", function (done) {
-    Q.spawn(function* (){
-      const searchTerm = 'cat';
-      const url = 'http://fakeUrl.com';
+describe('db-client', function () {
+  it('should retrieve a gif if url already exists', function (done) {
+    Q.spawn(function * () {
+      const searchTerm = 'cat'
+      const url = 'http://fakeUrl.com'
 
-      yield dBClient.delete(searchTerm);
+      yield dBClient.delete(searchTerm)
 
-      yield dBClient.save(searchTerm, url);
+      yield dBClient.save(searchTerm, url)
 
-      const retrievedUrl = yield dBClient.get(searchTerm);
-      retrievedUrl[0].should.be.equal(url);
-      done();
-    });
-  });
-});
+      const retrievedUrl = yield dBClient.get(searchTerm)
+      retrievedUrl[0].should.be.equal(url)
+      done()
+    })
+  })
+})
--- test/integration/giphinator-client-test.js
+++ test/integration/giphinator-client-test.js
@@ -1,19 +1,19 @@
-'use strict';
-const should = require("chai").should();
+'use strict'
+const should = require('chai').should()
 
-const GiphinatorClient = require("../../lib/external-clients/giphinator-client");
-const requestClient = require("../../lib/external-clients/request-client");
+const GiphinatorClient = require('../../lib/external-clients/giphinator-client')
+const requestClient = require('../../lib/external-clients/request-client')
 
-const giphinatorClient = new GiphinatorClient(requestClient);
+const giphinatorClient = new GiphinatorClient(requestClient)
 
-describe("giphinator-client", function () {
-  it("should retrieve gifs", function (done) {
-    const searchTerm = 'funny cat';
-    const limit = 10;
+describe('giphinator-client', function () {
+  it('should retrieve gifs', function (done) {
+    const searchTerm = 'funny cat'
+    const limit = 10
 
     giphinatorClient.getGifs(searchTerm, limit).then(urls => {
-      urls.length.should.be.equal(limit);
-      done();
-    });
-  });
-});
+      urls.length.should.be.equal(limit)
+      done()
+    })
+  })
+})
