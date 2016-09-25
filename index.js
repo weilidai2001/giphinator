b493187f64f4bd5e9978e658e6a0c1ad8664b12a
Weili Dai <Weili.Dai@sky.uk>
2016-09-25 22:29:18 +0100

    Added delete endpoint

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const express = require('express'),
-    giphinateHandler = require('./lib/handlers/giphinate'),
-    app = express()
+  giphinateHandler = require('./lib/handlers/giphinate'),
+  deleteCacheHandler = require('./lib/handlers/delete-cache-handler'),
+  app = express()
 
 app
-.get('/:queryText', giphinateHandler)
+  .get('/:queryText', giphinateHandler)
+  .delete('/:queryText', deleteCacheHandler)
 
-.listen(process.env.PORT, function () {
+  .listen(process.env.PORT, function () {
     console.log(`Listening on port ${process.env.PORT}`)
-})
+  })
